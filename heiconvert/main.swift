352048ef614036916734be413008f7a77c420ad9
Roman Ožana <roman@omdesign.cz>
2019-12-13 11:09:22 +0100

    Delet origin files

--- heiconvert/main.swift
+++ heiconvert/main.swift
@@ -7,27 +7,26 @@ import CoreImage
 let documentsUrl =  FileManager.default.urls(for: .downloadsDirectory, in: .userDomainMask).first!
 
 do {
-    // Get the directory contents urls (including subfolders urls)
-    let directoryContents = try FileManager.default.contentsOfDirectory(at: documentsUrl, includingPropertiesForKeys: nil)
-    let images = directoryContents.filter{ $0.pathExtension == "tif" }
+  // Get the directory contents urls (including subfolders urls)
+  let directoryContents = try FileManager.default.contentsOfDirectory(at: documentsUrl, includingPropertiesForKeys: nil)
+  let images = directoryContents.filter{ $0.pathExtension == "tif" }
+  
+  for imageUrl in images {
+    print("Converting file: ", imageUrl.deletingPathExtension().lastPathComponent)
+    let image = CIImage(contentsOf: imageUrl)
+    let context = CIContext(options: nil)
     
-    for imageUrl in images {
-        print("Converting file: ", imageUrl.deletingPathExtension().lastPathComponent)
-        let image = CIImage(contentsOf: imageUrl)
-        let context = CIContext(options: nil)
-        
-        let options = NSDictionary(dictionary: [kCGImageDestinationLossyCompressionQuality:0.76])
+    let options = NSDictionary(dictionary: [kCGImageDestinationLossyCompressionQuality:0.76])
+    
+    try! context.writeHEIFRepresentation(
+      of:image!,
+      to: imageUrl.deletingPathExtension().appendingPathExtension("heic"),
+      format: CIFormat.ARGB8,
+      colorSpace: image!.colorSpace!,
+      options:options as! [CIImageRepresentationOption : Any])
 
-        try! context.writeHEIFRepresentation(of:image!,
-                                to: imageUrl.deletingPathExtension().appendingPathExtension("heic"),
-                                format: CIFormat.ARGB8,
-                                colorSpace: image!.colorSpace!,
-                                options:options as! [CIImageRepresentationOption : Any])
-
-    }
-        
-
-
+    try FileManager.default.removeItem(at: imageUrl)
+  }
 } catch {
-    print(error)
+  print(error)
 }
