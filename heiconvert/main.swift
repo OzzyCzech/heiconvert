86c862c04334a787489993fce5553644d97dab93
Roman Ožana <roman@omdesign.cz>
2019-12-13 09:38:13 +0100

    Initial Commit

---
+++ heiconvert/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  heiconvert
+//
+//  Created by Roman Ožana on 13/12/2019.
+//  Copyright © 2019 Roman Ožana. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
